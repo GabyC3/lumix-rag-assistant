4029369a8dea6fbefae589a792dbbbfb1bae4827
GabyC3 <gabyrc69@gmail.com>
2026-07-25 13:18:08 -0300

    inicio del challenge agente

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Lumix AI Assistant",
+    page_icon="🤖",
+    layout="wide"
+)
+
+st.title("🤖 Lumix AI")
+
+st.markdown("""
+Bienvenido a **Lumix AI**.
+""")
